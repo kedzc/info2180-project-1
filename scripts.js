495d5600a7363ba7af865dcb8c88e814c6933ce1
Kedeisha Copeland <kedeisha.c@yahoo.com>
2020-11-02 01:53:43 -0500

    Correct Updated files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,9 @@
-/* Add your JavaScript to this file */
+document.getElementById("btn").onclick = function()
+{
+  const format =  /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+	if (email = re.test(email))
+    window.alert("Thank you! your email address " email " has been added to our mailing list");
+}
+else {
+    window.alert("Please enter valid email address")
+}
